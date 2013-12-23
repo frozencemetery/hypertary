00df1cbed8732c8bff21c1119a601536d8200149
Robbie Harwood (frozencemetery) <rharwood@club.cc.cmu.edu>
2013-12-23 04:24:21 -0500

    Improve flexibility of reporting default values.

--- backend/backend_standalone.py
+++ backend/backend_standalone.py
@@ -5,15 +5,15 @@ from wsgiref.simple_server import make_server
 
 import backend_wsgi as backend
 
+host = ""
+port = 8080
+
 # print help message and exit
 def help():
   print "syntax: " + sys.argv[0] + " [[HOST]:[PORT]]"
-  print "defaults to port 8080 and host \"\""
+  print "defaults to " + (host if host else "localhost") + ":" + str(port)
   sys.exit(1)
   pass
-
-host = ""
-port = 8080
 
 if "-h" in sys.argv or "--help" in sys.argv:
   help()
@@ -22,8 +22,8 @@ if "-h" in sys.argv or "--help" in sys.argv:
 if len(sys.argv) > 1:
   try:
     hp = sys.argv[1].split(":", 1)
-    host = hp[0] if hp[0] else host
     port = int(hp[1]) if hp[1] else port
+    host = hp[0] if hp[0] else host
     pass
   except:
     help()
